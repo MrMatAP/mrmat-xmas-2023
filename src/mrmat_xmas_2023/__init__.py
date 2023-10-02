5f305875a36733fb17c75ccdc7eededb1e4b227d
Mathieu Imfeld <imfeldma@gmail.com>
2023-10-02 18:10:28 +0200

    Replaced explicit credentials with config

--- src/mrmat_xmas_2023/__init__.py
+++ src/mrmat_xmas_2023/__init__.py
@@ -1,21 +1,25 @@
 import os
+import pathlib
 import importlib.metadata
 
 import msal
 import fastapi
 import fastapi.staticfiles
 
+from .config import Config
+
 try:
     __version__ = importlib.metadata.version('mrmat-xmas-2023')
 except importlib.metadata.PackageNotFoundError:
     # You have not yet installed this as a package, likely because you're hacking on it in some IDE
     __version__ = '0.0.0.dev0'
+__config_file__ = pathlib.Path('~/etc/mrmat-xmas-2023.yml').expanduser()
 
+config = Config(__config_file__)
 cc_app = msal.ConfidentialClientApplication(
-    authority='https://login.microsoftonline.com/common',
-    client_id='2231d20f-81a5-4107-acab-799c31ee2104',
-    client_credential='IjE8Q~pUl0vgU.szgXCF77fSGepdXZw8r4fBicBo'
-)
+    authority=config.authority,
+    client_id=config.client_id,
+    client_credential=config.client_secret)
 app = fastapi.FastAPI()
 
 
