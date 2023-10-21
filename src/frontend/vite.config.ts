ac317c71590940d8db38ad613f586e2af42f828f
Mathieu Imfeld <imfeldma@gmail.com>
2023-10-21 16:21:29 +0200

    exporting configuration

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 // import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
-defineConfig({
+export default defineConfig({
   plugins: [vue()],
   // resolve: {
   //   alias: {
